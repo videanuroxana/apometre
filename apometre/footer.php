1e56f3f5c8e953aec46f09876f3b8ef4e87ebbea
Roxana Videanu <videanuroxana@gmail.com>
2015-07-27 21:07:31 +0300

    added logout functionality

---
+++ apometre/footer.php
@@ -0,0 +1,12 @@
+<tr>
+				<td align="right">
+					Apometre v 0.0.1
+				</td>
+			</tr>		
+		
+		
+		
+		</table>
+	</body>
+</html>
+
---
+++ apometre/header.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Apometre</title>
+	
+	</head>
+	<body>
+	
+		<table style = "width:100%;height:100%"  border="2">
+			<tr>
+				<td>
+					Header
+				</td>
+			</tr>
+			
+			
+			
